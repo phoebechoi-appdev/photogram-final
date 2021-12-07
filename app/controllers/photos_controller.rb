98ffdbc142c4ee77bd717b816d69d6fcfcbdfa73
phoebechoi-uc <phoebechoi@uchicago.edu>
2021-12-07 23:16:57 +0000

    photo index and detail improved

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,7 @@ class PhotosController < ApplicationController
     matching_photos = Photo.all
 
     @list_of_photos = matching_photos.order({ :created_at => :desc })
-
+    @photos = Photo.joins(:poster).all.where({ :users => { :private => false } }).order({ :created_at => :desc })
     render({ :template => "photos/index.html.erb" })
   end
 
@@ -12,21 +12,19 @@ class PhotosController < ApplicationController
 
     matching_photos = Photo.where({ :id => the_id })
 
-    @the_photo = matching_photos.at(0)
+    @photo = matching_photos.at(0)
 
     render({ :template => "photos/show.html.erb" })
   end
 
   def create
-    the_photo = Photo.new
-    the_photo.owner_id = params.fetch("query_owner_id")
-    the_photo.caption = params.fetch("query_caption")
-    the_photo.image = params.fetch("query_image")
-    the_photo.comments_count = params.fetch("query_comments_count")
-    the_photo.likes_count = params.fetch("query_likes_count")
+    @photo = Photo.new
+    @photo.caption = params.fetch("query_caption")
+    @photo.image = params.fetch("query_image")
+    @photo.owner_id = @current_user.id
 
-    if the_photo.valid?
-      the_photo.save
+    if @photo.valid?
+      @photo.save
       redirect_to("/photos", { :notice => "Photo created successfully." })
     else
       redirect_to("/photos", { :notice => "Photo failed to create successfully." })
@@ -35,27 +33,24 @@ class PhotosController < ApplicationController
 
   def update
     the_id = params.fetch("path_id")
-    the_photo = Photo.where({ :id => the_id }).at(0)
+    @photo = Photo.where({ :id => the_id }).at(0)
 
-    the_photo.owner_id = params.fetch("query_owner_id")
-    the_photo.caption = params.fetch("query_caption")
-    the_photo.image = params.fetch("query_image")
-    the_photo.comments_count = params.fetch("query_comments_count")
-    the_photo.likes_count = params.fetch("query_likes_count")
+    @photo.caption = params.fetch("query_caption")
+    @photo.image = params.fetch("query_image")
 
-    if the_photo.valid?
-      the_photo.save
-      redirect_to("/photos/#{the_photo.id}", { :notice => "Photo updated successfully."} )
+    if @photo.valid?
+      @photo.save
+      redirect_to("/photos/#{@photo.id}", { :notice => "Photo updated successfully."} )
     else
-      redirect_to("/photos/#{the_photo.id}", { :alert => "Photo failed to update successfully." })
+      redirect_to("/photos/#{@photo.id}", { :alert => "Photo failed to update successfully." })
     end
   end
 
   def destroy
     the_id = params.fetch("path_id")
-    the_photo = Photo.where({ :id => the_id }).at(0)
+    @photo = Photo.where({ :id => the_id }).at(0)
 
-    the_photo.destroy
+    @photo.destroy
 
     redirect_to("/photos", { :notice => "Photo deleted successfully."} )
   end
