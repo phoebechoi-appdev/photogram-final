39e6b43666d59e97d3f634fe16a6321d9e2a3a21
phoebechoi-uc <phoebechoi@uchicago.edu>
2021-12-09 18:48:14 +0000

    comments id fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,23 +19,25 @@ class CommentsController < ApplicationController
 
   def create
     the_comment = Comment.new
-    the_comment.author_id = params.fetch(:query_author_id)
+    the_comment.author_id = @current_user.id
     the_comment.body = params.fetch(:query_body)
     the_comment.photo_id = params.fetch(:query_photo_id)
 
-    if the_comment.valid?
-      the_comment.save
-      redirect_to("/comments", { :notice => "Comment created successfully." })
-    else
-      redirect_to("/comments", { :notice => "Comment failed to create successfully." })
-    end
+    # if the_comment.valid?
+    #   the_comment.save
+    #   redirect_to("/comments", { :notice => "Comment created successfully." })
+    # else
+    #   redirect_to("/comments", { :notice => "Comment failed to create successfully." })
+    # end
+
+    redirect_to("/photos/#{comment.photo_id}")
   end
 
   def update
     the_id = params.fetch(:path_id)
     the_comment = Comment.where({ :id => the_id }).at(0)
 
-    the_comment.author_id = params.fetch(:query_author_id)
+    the_comment.author_id = @current_user.id
     the_comment.body = params.fetch(:query_body)
     the_comment.photo_id = params.fetch(:query_photo_id)
 
